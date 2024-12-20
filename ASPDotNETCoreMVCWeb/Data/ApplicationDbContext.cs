f39bd7ad758a6ec845d8cc1ffb5ce3ad058861b8
dabananda <imdmitra@gmail.com>
2024-12-20 16:39:41 +0600

    Database connection established, Category table created, populated Category table

---
+++ ASPDotNETCoreMVCWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ASPDotNETCoreMVCWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPDotNETCoreMVCWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData
+                (
+                    new Category { Id = 1, Name = "Web Development", DisplayOrder = 1 },
+                    new Category { Id = 2, Name = "AI/Machine Learning", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "Database", DisplayOrder = 3 },
+                    new Category { Id = 4, Name = "Programming", DisplayOrder = 4 },
+                    new Category { Id = 5, Name = "Network & Security", DisplayOrder = 5 }
+                );
+        }
+    }
+}
