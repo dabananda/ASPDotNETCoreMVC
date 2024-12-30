80c9a9ed72a9e87987a325d2b7175f0eb20f72f0
dabananda <imdmitra@gmail.com>
2024-12-30 13:15:35 +0600

    Added create category

--- ASPDotNETCoreMVCWeb/Controllers/CategoryController.cs
+++ ASPDotNETCoreMVCWeb/Controllers/CategoryController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPDotNETCoreMVCWeb.Data;
+using ASPDotNETCoreMVCWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPDotNETCoreMVCWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
         public IActionResult Index()
         {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
